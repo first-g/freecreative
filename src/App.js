8ac881ac1abdb17a39d261f71aa5e0a0e0f446b5
azimust <woody.azimbek@gmail.com>
2023-05-07 18:40:40 +0600

    add assets and update styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Gallery from './components/Azimbek/Gallery/Gallery';
+import Header from './components/Azimbek/Header';
 
 function App() {
   return (
-    <div className="App">
-      Вам нужен фирменный стиль или ребрендинг?
-    </div>
+    <>
+      <Header />
+      <Gallery/>
+    </>
   );
 }
 
