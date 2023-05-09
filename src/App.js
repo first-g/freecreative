3a2b0659e71e78e0879f485356a7e8cce94b33cd
azimust <woody.azimbek@gmail.com>
2023-05-09 18:21:59 +0600

    add Erbol component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Gallery from './components/Azimbek/Gallery/Gallery';
 import Guideline from './components/Azimbek/Guideline/Guideline';
 import Header from './components/Azimbek/Header/Header';
-import FormContact from './components/Erbol';
+import FormContact from './components/Erbol/index';
 
 function App() {
   return (
